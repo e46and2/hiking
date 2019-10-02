7f743f30e8924a5440ccd9ce9996eba93d58e085
e46and2 <e46and2.local>
2019-10-02 00:15:05 -0600

    added trails attrs

--- lib/hiking/trails.rb
+++ lib/hiking/trails.rb
@@ -1,8 +1,9 @@
 class Hiking::Trails
 
-attr_reader :featured_hikes, :gems
+attr_reader :featured_hikes, :gems, :state
 
 def initialize
+  @state = state 
   @featured_hikes = featured_hikes
   @gems = gems
 end
