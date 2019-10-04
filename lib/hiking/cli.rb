82ceda7d64af8c5abd6231d20690dbc969034911
Emily <e46and2@gmail.com>
2019-10-04 01:03:34 -0600

    Worked on possible solutions for the exit of the main menu when strings are entered; re-organized the order of CLI methods in the file.

--- lib/hiking/cli.rb
+++ lib/hiking/cli.rb
@@ -15,7 +15,6 @@ attr_reader :state, :trails
     puts "                                                                   "
     puts "To exit, type '000' and press the return key"
     puts "                                                                   "
-
     scrape_states
     list_states
     menu
@@ -24,8 +23,9 @@ attr_reader :state, :trails
   def menu
     @input = input
     input = nil
-    while input != 000
+    #input = Integer(gets) rescue false
 
+    while input != 000
       input = gets.chomp.to_i
       case input
       when 1..51
@@ -41,11 +41,56 @@ attr_reader :state, :trails
     end
   end
 
-  def goodbye
+  def state_sub_menu
     puts "                                                                   "
-    puts "Goodbye! Enjoy your hike!"
+    puts "Would you like more information about these featured trails?"
     puts "                                                                   "
-  end
+    puts "Enter 1 for yes, or 2 to return to the main menu."
+    puts "                                                                   "
+    response = gets.chomp.to_i
+    case response
+    when 1
+      list_trails
+      trail_sub_menu
+    when 2
+      list_states
+    else
+      invalid_entry
+      state_sub_menu
+      end
+    end
+
+    def trail_sub_menu
+      puts "                                                                   "
+      puts "Have you found your next hike yet?"
+      puts "If so, type 1 for yes, and press the return key TWICE"
+      puts "to exit the program and start hiking!"
+      puts "                                                                   "
+      puts "If you'd like to continue searching for hikes in a different state,"
+      puts "please press 2 to return to the main menu."
+      puts "                                                                   "
+      leave_or_stay = gets.chomp.to_i
+      case leave_or_stay
+      when 1
+        #response = "000"
+        #input = "000"
+        #do nothing, return to menu loop
+      when 2
+        list_states
+      else
+        invalid_entry
+        trail_sub_menu
+        end
+      end
+
+      def state_list_sub_menu
+        puts "                                                                   "
+        puts "What state would you like to hike in?"
+        puts "                                                                   "
+        puts "Enter the state number and press the return key,"
+        puts "or type '000' and press the return key to quit the program:"
+        puts "                                                                   "
+      end
 
   def scrape_states
     @scrape = Hiking::Scraper.scrape
@@ -84,59 +129,15 @@ attr_reader :state, :trails
     end
   end
 
-  def state_sub_menu
-    puts "                                                                   "
-    puts "Would you like more information about these featured trails?"
-    puts "                                                                   "
-    puts "Enter 1 for yes, or 2 to return to the main menu"
-    puts "                                                                   "
-    response = gets.chomp.to_i
-    case response
-    when 1
-      list_trails
-      trail_sub_menu
-    when 2
-      list_states
-    else
-      invalid_entry
-      end
-    end
-
-    def trail_sub_menu
-      puts "                                                                   "
-      puts "Have you found your next hike yet?"
-      puts "If so, type 1 for yes, and press the return key TWICE"
-      puts "to exit the program and start hiking!"
-      puts "                                                                   "
-      puts "If you'd like to continue searching for hikes in a different state,"
-      puts "please press 2 to return to the main menu"
-      puts "                                                                   "
-      leave_or_stay = gets.chomp.to_i
-      case leave_or_stay
-      when 1
-        #response = "000"
-        #input = "000"
-        #do nothing, return to menu loop
-      when 2
-        list_states
-      else
-        invalid_entry
-        end
-      end
-
-      def state_list_sub_menu
-        puts "                                                                   "
-        puts "What state would you like to hike in?"
-        puts "                                                                   "
-        puts "Enter the state number and press the return key,"
-        puts "or type '000' and press the return key to quit the program:"
-        puts "                                                                   "
-      end
-
       def invalid_entry
         puts "                                                                   "
         puts "**       Your entry wasn't valid. Please try again.       **       "
         puts "                                                                   "
       end
 
+      def goodbye
+        puts "                                                                   "
+        puts "Goodbye! Enjoy your hike!"
+        puts "                                                                   "
+      end
 end
