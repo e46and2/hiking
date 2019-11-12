3e524436ad0655ba251dfc12e48aabd965ea510d
Emily <e46and2@gmail.com>
2019-11-11 21:22:32 -0700

    Fixed the issue with typing letters on the main menu per the refactoring conversation from my CLI project review and also fixed a subsequent exit error.

--- lib/hiking/cli.rb
+++ lib/hiking/cli.rb
@@ -11,9 +11,7 @@ attr_reader :state, :trails
     puts "                Featured Hiking Trails in the US                   "
     puts "                --------------------------------                   "
     puts "                                                                   "
-    puts "Take a look at the list below to find the number for your selection"
-    puts "                                                                   "
-    puts "To exit, type '000' and press the return key"
+    puts "Take a look at the list below to find the number for your selection:"
     puts "                                                                   "
     scrape_states
     list_states
@@ -21,11 +19,9 @@ attr_reader :state, :trails
   end
 
   def menu
-    @input = input
-    input = nil
-    #input = Integer(gets) rescue false
-
-    while input != 000
+      @input = input
+      input = nil
+    while input != 77
       input = gets.chomp.to_i
       case input
       when 1..51
@@ -33,12 +29,17 @@ attr_reader :state, :trails
       puts "Your menu options are loading..."
       scrape_trails(input)
       state_sub_menu
-      when 000
+      when 77
       goodbye
       else
       invalid_entry
       end
     end
+  end
+
+  def main_menu_sub_menu
+      puts "Enter the number of the state to see featured trails,"
+      puts "or enter 77 to exit the program."
   end
 
   def state_sub_menu
@@ -63,18 +64,19 @@ attr_reader :state, :trails
     def trail_sub_menu
       puts "                                                                   "
       puts "Have you found your next hike yet?"
-      puts "If so, type 1 for yes, and press the return key TWICE"
-      puts "to exit the program and start hiking!"
       puts "                                                                   "
-      puts "If you'd like to continue searching for hikes in a different state,"
-      puts "please press 2 to return to the main menu."
+      puts "If not, press 2 to RETURN to the STATE MENU to continue searching."
+      puts "                                                                   "
+      puts "If so, press 77 to EXIT the program and start hiking! :) "
       puts "                                                                   "
       leave_or_stay = gets.chomp.to_i
+      puts "                                                                   "
       case leave_or_stay
-      when 1
-        #response = "000"
-        #input = "000"
-        #do nothing, return to menu loop
+      when 77
+        goodbye
+        begin
+          exit!
+        end
       when 2
         list_states
       else
@@ -88,7 +90,7 @@ attr_reader :state, :trails
         puts "What state would you like to hike in?"
         puts "                                                                   "
         puts "Enter the state number and press the return key,"
-        puts "or type '000' and press the return key to quit the program:"
+        puts "or type '77' and press the return key to quit the program:"
         puts "                                                                   "
       end
 
--- lib/hiking/scraper.rb
+++ lib/hiking/scraper.rb
@@ -13,14 +13,14 @@ self.scrape_states
 end
 
 def self.scrape_states
-@state_card.each_with_index do |info, i|
-number = i + 1
-state_name = info.css("div.card-block.area-info").css("h3.dont-shrink.serif").text
-total_featured_hikes = info.css("div.card-block.area-info").css("p").text.split("Hikes")[0]+"Hikes"
-total_trails = info.css("div.card-block.area-info").css("p").text.split("Hikes").last
-url = info.css("a").attribute("href").value
-state_info = Hiking::State.new(number, state_name, total_featured_hikes, total_trails, url)
-end
+  @state_card.each_with_index do |info, i|
+    number = i + 1
+    state_name = info.css("div.card-block.area-info").css("h3.dont-shrink.serif").text
+    total_featured_hikes = info.css("div.card-block.area-info").css("p").text.split("Hikes")[0]+"Hikes"
+    total_trails = info.css("div.card-block.area-info").css("p").text.split("Hikes").last
+    url = info.css("a").attribute("href").value
+    state_info = Hiking::State.new(number, state_name, total_featured_hikes, total_trails, url)
+  end
 end
 
 def self.scrape_trails(input)
